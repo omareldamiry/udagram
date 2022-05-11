d7c0da0ebb6421934e920c439379fd1e1e44a7f7
unknown <omar.eldamiry@gmail.com>
2022-05-11 15:40:13 +0200

    fixed backend deployment script

--- udagram-api/bin/deploy.sh
+++ udagram-api/bin/deploy.sh
@@ -1,3 +1,6 @@
+aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID
+aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY
+
 eb init udagram-api --platform node.js --region us-east-1
 eb create --sample udagram-api-dev
 eb use udagram-api-dev
