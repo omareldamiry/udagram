5680b1725e1d02f578875ddf757c875ab4d8a63b
unknown <omar.eldamiry@gmail.com>
2022-05-11 15:13:28 +0200

    modified deployment script

--- udagram-api/bin/deploy.sh
+++ udagram-api/bin/deploy.sh
@@ -1,14 +1,18 @@
-echo $POSTGRES_HOST
-echo $DB_PORT
-echo $PORT
-echo $POSTGRES_PASSWORD
-echo $POSTGRES_USERNAME
-echo $RDS_DIALECT
-echo $POSTGRES_DB
-echo $AWS_REGION
-echo $AWS_BUCKET
-echo $URL
-echo $AWS_ACCESS_KEY_ID
-echo $AWS_SECRET_ACCESS_KEY
-echo $JWT_SECRET
-sleep 5
+eb create --sample udagram-api-dev
+eb use udagram-api-dev
+
+eb setenv $POSTGRES_HOST
+eb setenv $DB_PORT
+eb setenv $PORT
+eb setenv $POSTGRES_PASSWORD
+eb setenv $POSTGRES_USERNAME
+eb setenv $RDS_DIALECT
+eb setenv $POSTGRES_DB
+eb setenv $AWS_REGION
+eb setenv $AWS_BUCKET
+eb setenv $URL
+eb setenv $AWS_ACCESS_KEY_ID
+eb setenv $AWS_SECRET_ACCESS_KEY
+eb setenv $JWT_SECRET
+
+eb deploy udagram-api-dev
