d3352c5f82582f46356ac4f799c94295abcf6db7
unknown <omar.eldamiry@gmail.com>
2022-05-12 05:27:18 +0200

    fixed frontend building script

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: true,
   appName: "Udagram",
-  apiHost: "udagram-api-dev.eba-peht7j28.us-east-1.elasticbeanstalk.com",
+  apiHost: "http://udagram-api-dev.eba-peht7j28.us-east-1.elasticbeanstalk.com",
 };
 
 /*
